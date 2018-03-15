fc437e4f0b4c4a6c4ac4ca6f062aac1f109d6217
Moisés Vilar <mvilar@gradiant.org>
2018-03-15 21:22:44 +0100

    Added text to 'historia' section

--- launch-docker.bat
+++ launch-docker.bat
@@ -1,1 +1,1 @@
-docker run -d --name little-bardo -v %cd%\public\:/usr/share/nginx/html -p 80:80 nginx:alpine
+docker run -d --name little-bardo -v /c/Users/Mois/littlebardo/scu-landing/public/:/usr/share/nginx/html -p 80:80 nginx:alpine
